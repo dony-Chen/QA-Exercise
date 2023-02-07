512989ba137f2feba5ef86250711e6207e13d5c9
dony <jerrybearooxx05@gmail.com>
2023-02-08 03:18:12 +0800

    v1.1

--- automatedTesting/automatedTesting.py
+++ automatedTesting/automatedTesting.py
@@ -13,7 +13,10 @@ driver.implicitly_wait(5)
 i += 1
 driver.find_element_by_class_name('cubre-o-header__burger').click()
 #driver.find_element_by_css_selector(.cubre-o-nav__menu .cubre-o-menu__item:nth-child(1)).click()
-driver.find_element_by_class_name('cubre-o-menu__item').click()
+driver.find_element_by_xpath('//div[3]/div/div[2]/div/div/div/div/div').click()
+#driver.find_element_by_class_name('cubre-o-menu__item:nth-child(1)').click().click()
+#driver.find_element_by_class_name('cubre-o-menu__btn').click()
+#driver.find_element_by_class_name('cubre-a-menuSortBtn').click()
 #div = driver.find_element_by_id('i_am_an_id')
 
 
