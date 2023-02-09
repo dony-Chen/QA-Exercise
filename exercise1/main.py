3eecfe242f5603d86cc84a0802c8585e90019065
dony-Chen <jerrybearooxx05@gmail.com>
2023-02-09 10:22:12 +0800

    V3

--- exercise1/main.py
+++ exercise1/main.py
@@ -1,9 +1,16 @@
 def isNumEligible(num):
   if ((num % 2 == 1) or (num > 5  and num < 21)):
     print("X")
+    return True
+  elif num < 2:
+    print("請不要輸入小於二的偶數～") 
+    return False
   else:
     print("O")
+    return True
 
-  
-num = input("請輸入數字: ")
-isNumEligible(int(num))
+access = False
+
+while(not access):  
+  num = input("請輸入數字: ")
+  access = isNumEligible(int(num))
